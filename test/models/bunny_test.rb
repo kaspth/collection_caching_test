7e5feb779cfc5cee47523675ca3f492b05bab867
Kasper Timm Hansen <kaspth@gmail.com>
2016-01-08 20:54:28 +0100

    Add the bunny test.

--- test/models/bunny_test.rb
+++ test/models/bunny_test.rb
@@ -4,4 +4,22 @@ class BunnyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  100.times do |n|
+    test "this thing works #{n}" do
+      assert true
+    end
+  end
+
+  test "something failing" do
+    assert false, 'This failed'
+  end
+
+  test "error me" do
+    raise ArgumentError, "Wups! Bet you didn't expect this!"
+  end
+
+  test "skip me" do
+    skip "I would like to be skipped"
+  end
 end
